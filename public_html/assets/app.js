db8fc2e221b7ab3b16629210a49bddfc018d7749
Kevin Mullin <kevin@kpmullin.com>
2013-02-28 13:54:51 -0800

    js cleanup; support for read-only links

--- public_html/assets/app.js
+++ public_html/assets/app.js
@@ -4,28 +4,40 @@ var uri_map = {
   dir_api: '/a/dir/'
 };
 
+var link_map = {
+  "URI:CHK:": uri_map.file_id,
+  "URI:DIR2:": uri_map.dir_id,
+  "URI:DIR2-RO:": uri_map.dir_id + 'ro/'
+};
+
 function get_link(lafs_uri) {
-  var link = '';
-  if (lafs_uri.substr(0, 8) == "URI:CHK:") {
-    // file ro-cap
-    link = uri_map.file_id + lafs_uri.substr(8);
+  for (var k in link_map) {
+    if (lafs_uri.substr(0, k.length) == k)
+      return link_map[k] + lafs_uri.substr(k.length).replace(/:/g, '/');
   }
-  else if (lafs_uri.substr(0, 9) == "URI:DIR2:") {
-    // dir rw-cap
-    link = uri_map.dir_id + lafs_uri.substr(9);
-  }
-  return link.replace(/:/g, '/');
+}
+
+function gen_unlink_button(filename, post_uri, return_to) {
+  var html_form = new Array();
+  html_form.push('<form action="' + post_uri +'" method="post">');
+  html_form.push('<input type="hidden" name="t" value="unlink" />');
+  html_form.push('<input type="hidden" name="name" value="' + filename + '" />');
+  html_form.push('<input type="hidden" name="when_done" value="' + return_to + '" />');
+  html_form.push('<input type="submit" name="unlink" value="unlink" />');
+  html_form.push('</form>');
+  return html_form.join('');
 }
 
 function make_directory_grid(currentId) {
   var child_rows = new Array();
   // make a call for json data at api uri
   $.getJSON(uri_map.dir_api + currentId, function(data) {
-    var children = data[1]['children'];
+    var children = data[1].children;
     var filename = null;
     var size = null;
     var obj_uri = null;
     var type = null;
+    var name_link = null;
     var ctime = new Date();
     // for each filenode or dirnode fill Array with rows
     $.each(children, function(name, child_d) {
@@ -39,11 +51,13 @@ function make_directory_grid(currentId) {
       if (child_d[0] == 'filenode')
         filename = '/' + name;
       type = (child_d[0] == 'filenode') ? 'file' : 'directory';
-      name = '<a href="' + escape(get_link(obj_uri) + filename) + '">' + name + '</a>';
+      name_link = '<a href="' + escape(get_link(obj_uri) + filename) + '">' + name + '</a>';
+      // add delete link if its not ro
+      name_link += (currentId.substr(0,3) == "ro/") ? '' : gen_unlink_button(name, uri_map.dir_api + currentId, uri_map.dir_id + currentId);
       ctime = new Date(Math.round(child_d[1].metadata.tahoe.linkcrtime*1000));
       ctime = ctime.toISOString();
       ctime = '<abbr class="timeago" title="' + ctime + '">' + ctime + '</abbr>';
-      child_rows.push([name, type, size, ctime]);
+      child_rows.push([name_link, type, size, ctime]);
     });
     // if nothing in array dont make table or show upload stuffs
     if (child_rows.index != 0) {
@@ -63,19 +77,20 @@ function make_directory_grid(currentId) {
       });
       $("form").attr("action", uri_map.dir_api + currentId);
       $("input.return_to").attr("value", location.pathname);
-      $("div.upload-form").show();
       $("abbr.timeago").timeago();
+      if (currentId.substr(0,3) != "ro/")
+        $("div.upload-form").show();
     }
   });
 }
 
 $(document).ready(function() {
 
-  // dynamically create regex for matching api uris
-  var re = new RegExp("^" + uri_map.dir_id.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1") + "\\w{26}\\/\\w{52}\\/?$","g");
-  if ( re.test(location.pathname) ) {
-    // if directory
-    make_directory_grid(location.pathname.replace(uri_map.dir_id, ''));
+  // dynamically create regex for matching view (dirs) uris
+  var re = new RegExp("^" + uri_map.dir_id.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1") + "((?:ro/)?\\w{26}\\/\\w{52})\\/?$","g");
+  var matched_id = re.exec(location.pathname);
+  if ( matched_id != null ) {
+    make_directory_grid(matched_id[1]);
   }
 
 });
