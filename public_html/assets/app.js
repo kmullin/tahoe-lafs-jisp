fec580b327da8a1bcd2ed994f15c7c7e1a09e690
Kevin Mullin <kevin@kpmullin.com>
2013-02-28 23:29:13 -0800

    set site title and heading upon loading a directory

--- public_html/assets/app.js
+++ public_html/assets/app.js
@@ -98,7 +98,10 @@ function make_directory_grid(currentId) {
     var child_rows = fill_grid_rows(data[1].children, currentId);
     // if nothing in array dont make table or show upload stuffs
     if (child_rows.index != 0) {
-      $("h3.heading").text("Directory: " + data[1].verify_uri.substr(18, 5)).show();
+      var title = 'Directory: ' + data[1].verify_uri.substr(18, 5);
+      title += (currentId.substr(0,3) == 'ro/') ? ' (ro)' : '';
+      document.title = title;
+      $("h3.heading").text(title).show();
       light_up_table(child_rows, currentId); // init table
       $("form").attr("action", uri_map.dir_api + currentId);
       $("input.return_to").attr("value", location.pathname);
