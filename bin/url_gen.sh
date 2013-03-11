81d20d90b2acc8256b7b8affc926201d02fea5b1
Kevin Mullin <kevin@kpmullin.com>
2013-03-10 17:17:51 -0700

    explain script a little bit

--- bin/url_gen.sh
+++ bin/url_gen.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#
+# Usage:
+# $ DOMAIN=example.com ./url_gen.sh URI:DIR2-RO:gdfys4rrb421cqprha46tu3edm:wwyfx52w7fre7ujbczcny6dy374ozihwarwjwkgpy77ymxcwkita
+# https://example.com/d/ro/wwyfx52w7fre7ujbczcny6dy374ozihwarwjwkgpy77ymxcwkita/gdfys4rrb421cqprha46tu3edm/ (REVERSE=true)
+# https://example.com/d/ro/gdfys4rrb421cqprha46tu3edm/wwyfx52w7fre7ujbczcny6dy374ozihwarwjwkgpy77ymxcwkita/ (REVERSE=false)
+#
+# Make sure to change REVERSE to the appropriate value if you want URI's reversed or not
 
-DOMAIN=YOUR.DOMAIN.HERE
 REVERSE=true
 
 URL="https://${DOMAIN}/d"
